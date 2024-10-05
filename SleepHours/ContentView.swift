3080ecc5549bf934a9b1fdecc2f48543c32e5b1b
andydangit <andy.dang.it@gmail.com>
2024-10-05 14:28:59 -0500

    Add: add date time format

--- SleepHours/ContentView.swift
+++ SleepHours/ContentView.swift
@@ -14,19 +14,37 @@ struct ContentView: View {
     
     var body: some View {
         
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
-        
-        
-//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 5...12, step: 0.25)
+//        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+//            .labelsHidden()
 //        
+//            .padding()
+//        
+//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 5...12, step: 0.25)
+        
+        Text(Date.now, format: .dateTime.hour().minute())
+        Text(Date.now, format: .dateTime.day().month().year())
+        Text(Date.now.formatted(date: .long, time: .shortened))
+        
         
     }
     
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        let now = Date.now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = Date.now...tomorrow
+        
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
+    
+
 }
 
 #Preview {
