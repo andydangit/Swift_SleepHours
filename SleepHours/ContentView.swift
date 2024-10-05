a0e17d5761dbb63b661b865215d38dbff081a604
andydangit <andy.dang.it@gmail.com>
2024-10-05 13:49:20 -0500

    Add: Setting date picker for tomorrow

--- SleepHours/ContentView.swift
+++ SleepHours/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 5...12, step: 0.25)
+        
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+        
+        
+//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 5...12, step: 0.25)
+//        
+        
+    }
+    
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
