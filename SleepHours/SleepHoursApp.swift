b29934eaeb830fc195b7dea2684006c6a3edb3b0
andydangit <andy.dang.it@gmail.com>
2024-10-05 11:02:22 -0500

    Initial Commit

---
+++ SleepHours/SleepHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepHoursApp.swift
+//  SleepHours
+//
+//  Created by Andy Dang It on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
