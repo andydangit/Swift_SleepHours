7a270f16d8f6b16e4401e69cb040a88b8e50f212
andydangit <andy.dang.it@gmail.com>
2024-10-06 10:59:27 -0500

    Add: basic UI layout design for the sleep calculator

--- SleepHours/ContentView.swift
+++ SleepHours/ContentView.swift
@@ -11,40 +11,40 @@ struct ContentView: View {
     
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        
-//        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-//            .labelsHidden()
-//        
-//            .padding()
-//        
-//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 5...12, step: 0.25)
-//        
-//        Text(Date.now, format: .dateTime.hour().minute())
-//        Text(Date.now, format: .dateTime.day().month().year())
-//        Text(Date.now.formatted(date: .long, time: .shortened))
-        Text("test")
+        NavigationStack {
+            VStack {
+                
+                //When to wake up
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                // Desire to Sleep
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...7, step: 0.25)
+                
+                // Coffee Amount
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cups", value: $coffeeAmount, in: 0...11)
+                
+            }
+            .navigationTitle("Sleep Hours")
+            .toolbar {
+                Button("calculate", action: calculateBedTime)
+            }
+        }
+    }
+    
+    func calculateBedTime() {
         
     }
     
-    func exampleDates() {
-//        let now = Date.now
-//        let tomorrow = Date.now.addingTimeInterval(86400)
-//        let range = Date.now...tomorrow
-        
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-//        
-//        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-//        let hour = components.hour ?? 0
-//        let minute = components.minute ?? 0
-        
-    }
-    
-
 }
 
 #Preview {
